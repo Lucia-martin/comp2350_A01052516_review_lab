8cfccd4d5c776a4016c4293399c5507bd6cee42c
Lucia-martin <90855217+Lucia-martin@users.noreply.github.com>
2022-02-28 12:09:55 -0800

    Update databaseConnection.js

--- databaseConnection.js
+++ databaseConnection.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2');
 const is_heroku = process.env.IS_HEROKU || false;
 
 const dbConfigHeroku = {
-	host: "ckshdphy86qnz0bj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-	user: "vg6pkxehelco2587",
-	password: "dn6rx84pfpudqktr",
-	database: "kqvbwep73wbpmutu",
+	host: "ble5mmo2o5v9oouq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+	user: "b7cp5wy1r0h0z3iz",
+	password: "qtcszhs6l9jdhvau",
+	database: "d0u2shx1pv2ca085",
 	multipleStatements: false, 
     namedPlaceholders:true
 };
